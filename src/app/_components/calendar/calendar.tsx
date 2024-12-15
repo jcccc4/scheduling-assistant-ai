e0e08f237dbf00a8b45b83f2759b7b50a1f9d8f9
JC <jcsacra4@gmail.com>
2024-12-16 02:08:23 +0800

    added render view

--- src/app/_components/calendar/calendar.tsx
+++ src/app/_components/calendar/calendar.tsx
@@ -172,11 +172,13 @@ export default function Scheduler() {
       handleTask={handleTask}
       
     >
-      <WeeklyView
-            handleTask={handleTask}
-            tasks={optimisticTasks}
-            selectedDate={selectedDate}
-          />
+      {isLoading ? (
+        <div className="flex items-center justify-center h-full">
+          <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        renderView()
+      )}
     </CalendarLayout>
   );
 }
